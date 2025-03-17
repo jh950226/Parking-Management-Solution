558c7d6251a5b78fafc245efc13c7dc604438463
YOO JAE HYUN <deep1995@koreatech.ac.kr>
2025-03-17 22:13:23 +0900

    Add RFID_TEST_Arduino

---
+++ iot_controller/RFID_TEST.ino
@@ -0,0 +1,28 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN 9
+#define SS_PIN 10
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println("Scan an RFID card...");
+}
+
+void loop() {
+  if (!mfrc522.PICC_IsNewCardPresent()) return;
+  if (!mfrc522.PICC_ReadCardSerial()) return;
+
+  // UID 전송 (접두사 없이 UID만 전송)
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.println();  // UID 출력 후 줄바꿈
+
+  delay(1000);  // 중복 인식을 방지하기 위한 약간의 지연
+}
